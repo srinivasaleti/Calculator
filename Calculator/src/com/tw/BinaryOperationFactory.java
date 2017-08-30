6f65688cd8482745ef583d2d47e541d08a2b8ce5
srinivasarao <srinivra@thoughtworks.com>
2017-08-30 17:56:46 +0530

    Extract magic variables

--- Calculator/src/com/tw/BinaryOperationFactory.java
+++ Calculator/src/com/tw/BinaryOperationFactory.java
@@ -3,17 +3,22 @@ package com.tw;
 //Responsible for performing proper operation
 class BinaryOperationFactory {
 
+    private static final char PLUS = '+';
+    private static final char MINUS = '-';
+    private static final char PRODUCT = '*';
+    private static final char DIVISION = '/';
+
     BinaryOperation getOperation(Operator operator, Operand firstOperand, Operand secondOperand) {
-        if (operator.equals(new Operator('+'))) {
+        if (operator.equals(new Operator(PLUS))) {
             return new Addition(firstOperand, secondOperand);
         }
-        if (operator.equals(new Operator('-'))) {
+        if (operator.equals(new Operator(MINUS))) {
             return new Subtraction(firstOperand, secondOperand);
         }
-        if (operator.equals(new Operator('*'))) {
+        if (operator.equals(new Operator(PRODUCT))) {
             return new Multiplication(firstOperand, secondOperand);
         }
-        if (operator.equals(new Operator('/'))) {
+        if (operator.equals(new Operator(DIVISION))) {
             return new Division(firstOperand, secondOperand);
         }
         throw new InvalidOperationException("Invalid Operation");
