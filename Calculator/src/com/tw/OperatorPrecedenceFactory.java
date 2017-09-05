35b65f65a9e36b24589527f301b2e52bbf51fc18
srinivasarao <srinivra@thoughtworks.com>
2017-09-05 08:08:27 +0530

    Add a method to get precedence of an operator

--- Calculator/src/com/tw/OperatorPrecedenceFactory.java
+++ Calculator/src/com/tw/OperatorPrecedenceFactory.java
@@ -3,4 +3,29 @@ package com.tw;
 //Represents a factory which gives precedence
 class OperatorPrecedenceFactory {
 
+    private static final Operator ADDITIVE_OPERATOR = new Operator('+');
+    private static final Operator SUBTRACTIVE_OPERATOR = new Operator('-');
+    private static final Operator MULTIPLICATIVE_OPERATOR = new Operator('/');
+    private static final Operator DIVISION_OPERATOR = new Operator('*');
+
+    private static final Precedence ADDITIVE_SUBTRACTIVE_PRECEDENCE = new Precedence(1);
+    private static final Precedence DIVISION_MULTIPLICATIVE_PRECEDENCE = new Precedence(2);
+    private static final Precedence MINIMUM_PRECEDENCE = new Precedence(0);
+
+    Precedence getPrecedence(Operator operator) {
+        if (operator.equals(ADDITIVE_OPERATOR)) {
+            return ADDITIVE_SUBTRACTIVE_PRECEDENCE;
+        }
+        if (operator.equals(SUBTRACTIVE_OPERATOR)) {
+            return ADDITIVE_SUBTRACTIVE_PRECEDENCE;
+        }
+        if (operator.equals(MULTIPLICATIVE_OPERATOR)) {
+            return DIVISION_MULTIPLICATIVE_PRECEDENCE;
+        }
+        if (operator.equals(DIVISION_OPERATOR)) {
+            return DIVISION_MULTIPLICATIVE_PRECEDENCE;
+        }
+        return MINIMUM_PRECEDENCE;
+    }
+
 }
--- Calculator/test/com/tw/OperatorPrecedenceFactoryTest.java
+++ Calculator/test/com/tw/OperatorPrecedenceFactoryTest.java
@@ -1,6 +1,52 @@
 package com.tw;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class OperatorPrecedenceFactoryTest {
 
-}
+    private OperatorPrecedenceFactory operatorPrecedenceFactory;
 
+    @BeforeEach
+    void setUp() {
+        this.operatorPrecedenceFactory = new OperatorPrecedenceFactory();
+    }
+
+    @Test
+    void expectedPrecedenceOfAdditiveOperator() {
+        Precedence precedence = new Precedence(1);
+        Operator operator = new Operator('+');
+        assertEquals(precedence, this.operatorPrecedenceFactory.getPrecedence(operator));
+    }
+
+    @Test
+    void expectedPrecedenceOfSubtractiveOperator() {
+        Precedence precedence = new Precedence(1);
+        Operator operator = new Operator('-');
+        assertEquals(precedence, this.operatorPrecedenceFactory.getPrecedence(operator));
+    }
+
+    @Test
+    void expectedPrecedenceOfMultiplicativeOperator() {
+        Precedence precedence = new Precedence(2);
+        Operator operator = new Operator('*');
+        assertEquals(precedence, this.operatorPrecedenceFactory.getPrecedence(operator));
+    }
+
+    @Test
+    void expectedPrecedenceOfDivisionOperator() {
+        Precedence precedence = new Precedence(2);
+        Operator operator = new Operator('/');
+        assertEquals(precedence, this.operatorPrecedenceFactory.getPrecedence(operator));
+    }
+
+    @Test
+    void expectedMinimumPrecedence() {
+        Precedence precedence = new Precedence(0);
+        Operator operator = new Operator('i');
+        assertEquals(precedence, this.operatorPrecedenceFactory.getPrecedence(operator));
+    }
+
+}
