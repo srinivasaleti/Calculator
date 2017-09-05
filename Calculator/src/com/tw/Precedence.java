16f702e278e2e4d6c5354da4ff58cc1d0841abe7
srinivasarao <srinivra@thoughtworks.com>
2017-09-05 07:51:22 +0530

    Add a method to verify equality of two precedence

--- Calculator/src/com/tw/Precedence.java
+++ Calculator/src/com/tw/Precedence.java
@@ -3,4 +3,25 @@ package com.tw;
 //Represents precedence
 class Precedence {
 
+    private final int value;
+
+    Precedence(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (this.getClass() != object.getClass()) {
+            return false;
+        }
+        if (this == object) {
+            return true;
+        }
+        Precedence that = (Precedence) object;
+        return this.value == that.value;
+    }
+
 }
--- Calculator/test/com/tw/PrecedenceTest.java
+++ Calculator/test/com/tw/PrecedenceTest.java
@@ -1,5 +1,39 @@
 package com.tw;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 class PrecedenceTest {
+
+    private Precedence precedence;
+
+    @BeforeEach
+    void setUp() {
+        this.precedence = new Precedence(1);
+    }
+
+    @Test
+    void precedenceIsNotEqualToNull() {
+        assertNotEquals(null, this.precedence);
+    }
+
+    @Test
+    void precedenceIsNotEqualToString() {
+        assertNotEquals("Precedence", this.precedence);
+    }
+
+    @Test
+    void precedenceIsEqualToItSelf() {
+        assertEquals(this.precedence, this.precedence);
+    }
+
+    @Test
+    void precedenceIsEqualToSamePrecedence() {
+        Precedence precedence = new Precedence(1);
+        assertEquals(precedence, this.precedence);
+    }
 
 }
